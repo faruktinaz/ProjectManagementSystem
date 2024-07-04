d37e0d36df1640bd9930dccccba8e9b1d576164d
faruktinaz <faruktinazz@icloud.com>
2024-07-05 02:04:03 +0300

    update admin panel for project model

--- project_management_system/projects/admin.py
+++ project_management_system/projects/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description', 'language')
+    prepopulated_fields = {'slug': ('name',)}
