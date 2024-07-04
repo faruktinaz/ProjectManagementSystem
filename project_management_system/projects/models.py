213b66cb77157d3ec2e33c7afceb7eef5ec08ae8
faruktinaz <faruktinazz@icloud.com>
2024-07-05 01:23:20 +0300

    add comment line for project model details

--- project_management_system/projects/models.py
+++ project_management_system/projects/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# add new projects with details such as name, slug, description, and language.
 class Project(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
