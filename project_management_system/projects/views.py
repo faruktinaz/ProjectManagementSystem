5c5d2ad6e2a1df2c2d98ec985297616cae5648db
faruktinaz <faruktinazz@icloud.com>
2024-07-05 01:22:09 +0300

    add ProjectViewSet for handling CRUD operations

--- project_management_system/projects/views.py
+++ project_management_system/projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
 
-# Create your views here.
+# I learned that I don't need to write separate views for CRUD operations.
+# All operations can be done in a single ViewSet.
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
