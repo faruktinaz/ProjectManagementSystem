e5395902c04165f8f3a670c442e0bf8758b832a2
faruktinaz <faruktinazz@icloud.com>
2024-07-05 02:03:08 +0300

    used router for automatically generate urlsViewSets

---
+++ project_management_system/projects/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet
+
+router = DefaultRouter()
+router.register('projects', ProjectViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
