74d2fbceadf6ecacd1744148aa5818e88697ea32
faruktinaz <faruktinazz@icloud.com>
2024-07-05 23:12:40 +0300

    import axios for making http requests to server

---
+++ project_management_system/frontend/src/components/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-data-table :headers="headers" :items="items"></v-data-table>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      items: [],
+      headers: [
+        { text: 'Name', value: 'name' },
+        { text: 'Description', value: 'description' },
+      ],
+    };
+  },
+  mounted() {
+    this.fetchProjects();
+  },
+  methods: {
+    async fetchProjects() {
+      try {
+        const response = await axios.get('http://localhost:8000/api/projects/');
+        this.items = response.data;
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    },
+  },
+};
+</script>
