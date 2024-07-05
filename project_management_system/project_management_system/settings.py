89a7d72ca66206ac08ac5676898aad888701660d
faruktinaz <faruktinazz@icloud.com>
2024-07-05 23:23:17 +0300

    Add CORS allowed methods configuration

--- project_management_system/project_management_system/settings.py
+++ project_management_system/project_management_system/settings.py
@@ -42,7 +42,23 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
