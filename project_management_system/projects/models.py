367abf3b8e03887c7f3906f94e1dd35af92abdc4
faruktinaz <faruktinazz@icloud.com>
2024-07-06 03:21:28 +0300

    created Repository db model (title, url, type, email, token)

--- project_management_system/projects/models.py
+++ project_management_system/projects/models.py
@@ -6,3 +6,14 @@ class Project(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
     language = models.CharField(max_length=50)
+    
+class Repository(models.Model):
+    TYPES = [
+		('github', 'GitHub'), ('gitlab','GitLab'), ('bitbucket','Bitbucket')
+	]
+    project = models.ForeignKey(Project, related_name='repositories', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    type = models.CharField(choices=TYPES, max_length=10)
+    email = models.EmailField()
+    token = models.CharField()
