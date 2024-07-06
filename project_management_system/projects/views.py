0d45d1150b0aec24d96d42c01727bbd29fe65b5c
faruktinaz <faruktinazz@icloud.com>
2024-07-06 03:23:24 +0300

    RepositoryViewSet for crud operations

--- project_management_system/projects/views.py
+++ project_management_system/projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Project
-from .serializers import ProjectSerializer
+from .models import Project, Repository
+from .serializers import ProjectSerializer, RepositorySerializer
 
 # I learned that I don't need to write separate views for CRUD operations.
 # All operations can be done in a single ViewSet.
@@ -9,3 +9,7 @@ from .serializers import ProjectSerializer
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+class RepositoryViewSet(viewsets.ModelViewSet):
+    queryset = Repository.objects.all()
+    serializer_class = RepositorySerializer
