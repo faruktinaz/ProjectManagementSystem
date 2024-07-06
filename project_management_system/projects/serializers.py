4d4760f2a322516f8bbf8d731f0aeec3edf5f525
faruktinaz <faruktinazz@icloud.com>
2024-07-06 03:22:21 +0300

    added RepositorySerializer

--- project_management_system/projects/serializers.py
+++ project_management_system/projects/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Repository
 
+class RepositorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Repository
+        fields = '__all__'
+        
 class ProjectSerializer(serializers.ModelSerializer):
+    repositories = RepositorySerializer(many=True)
     class Meta:
         model = Project
         fields = '__all__'
