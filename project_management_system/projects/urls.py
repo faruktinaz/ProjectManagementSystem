46ff2ab086a43fc7d58f914c6c019dc52d12866b
faruktinaz <faruktinazz@icloud.com>
2024-07-06 03:32:56 +0300

    router register trackers for rest framework router

--- project_management_system/projects/urls.py
+++ project_management_system/projects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet, RepositoryViewSet
+from .views import ProjectViewSet, RepositoryViewSet, TrackerViewSet
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet)
 router.register('repositories', RepositoryViewSet)
+router.register('trackers', TrackerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
