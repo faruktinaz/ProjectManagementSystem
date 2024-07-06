22661a5ce35f04161a1bc599af2ae6d8fd1597d9
faruktinaz <faruktinazz@icloud.com>
2024-07-06 03:24:05 +0300

    router register RepositoryViewSet

--- project_management_system/projects/urls.py
+++ project_management_system/projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewSet
+from .views import ProjectViewSet, RepositoryViewSet
 
 router = DefaultRouter()
 router.register('projects', ProjectViewSet)
+router.register('repositories', RepositoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
