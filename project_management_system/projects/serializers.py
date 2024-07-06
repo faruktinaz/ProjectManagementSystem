e26c420e7576967204f04fbb03196833c58a4033
faruktinaz <faruktinazz@icloud.com>
2024-07-06 03:31:19 +0300

    added TrackerSerializer, update projectSerializer for trackers

--- project_management_system/projects/serializers.py
+++ project_management_system/projects/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
-from .models import Project, Repository
+from .models import Project, Repository, Tracker
 
 class RepositorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Repository
         fields = '__all__'
         
+class TrackerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tracker
+        fields = '__all__'
+
 class ProjectSerializer(serializers.ModelSerializer):
     repositories = RepositorySerializer(many=True)
+    trackers = TrackerSerializer(many=True)
     class Meta:
         model = Project
         fields = '__all__'
